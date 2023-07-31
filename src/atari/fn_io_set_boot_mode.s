fa5d862ff3d1d5ec246576466c67aded04f36b4b
Mark Fisher <mark.j.fisher@gmail.com>
2023-07-31 08:43:14 +0100

    Add _fn_io_set_boot_mode

---
+++ src/atari/fn_io_set_boot_mode.s
@@ -0,0 +1,22 @@
+        .export     _fn_io_set_boot_mode
+
+        .include    "atari.inc"
+        .include    "zeropage.inc"
+        .include    "../inc/macros.inc"
+        .include    "fn_io.inc"
+        .import     _fn_io_copy_dcb
+
+; void _fn_io_set_boot_mode(uint8 mode)
+.proc _fn_io_set_boot_mode
+        sta     tmp1    ; save mode
+
+        setax   #t_io_set_boot_mode
+        jsr     _fn_io_copy_dcb
+
+        mva     tmp1, IO_DCB::daux1
+        jmp     SIOV
+.endproc
+
+.rodata
+t_io_set_boot_mode:
+        .byte $d9, $00, $00, $00, $0f, $00, $00, $00, $ff, $00
