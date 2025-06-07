1d07813ca3bf93f115657bc80c9dcf9fea055f01
Mark Fisher <mark.j.fisher@gmail.com>
2025-06-07 20:38:35 +0100

    Only do paging if we have banked ram available

--- src/atari/full/modules/mod_files/mod_files.s
+++ src/atari/full/modules/mod_files/mod_files.s
@@ -3,14 +3,19 @@
         .import     _mf_init_screen
         .import     mfp_main
         .import     mfs_main
+        .import     _bank_count
 
 .proc _mod_files
         jsr     _mf_init_screen
 
-        ; this is "mod files - simple", more complex block reading not yet implemented
-        ; jmp     mfs_main
-
-        ; shiny new "mod files - paging" - eventually make this picked from config
+        ; Choose between simple and paging version based on available banked RAM
+        lda     _bank_count
+        beq     use_simple      ; No banks available - use simple version
+        
+        ; Banks available - use paging version with cache
         jmp     mfp_main
-        ; rts
+        
+use_simple:
+        ; No banked RAM - use simple version
+        jmp     mfs_main
 .endproc
